c25c668ab126cdbeddb50a5d0d0309c99679437a
James Robb <47126579+jamesrweb@users.noreply.github.com>
2023-04-19 16:01:32 +0200

    Add simple documentation and types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'node:path'
 
 import {defineConfig, splitVendorChunkPlugin} from 'vite'
+import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [dts(), react(), splitVendorChunkPlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src', 'main.tsx'),
