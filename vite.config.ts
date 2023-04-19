b363234b032c5e00e6058dafa859dd10434c5df0
James Robb <47126579+jamesrweb@users.noreply.github.com>
2023-04-19 16:35:21 +0200

    Add externals to rollup options
    Signed-off-by: James Robb <47126579+jamesrweb@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,16 @@ export default defineConfig({
       entry: resolve(__dirname, 'src', 'main.tsx'),
       name: 'NextReactP5Wrapper',
     },
+   rollupOptions: {
+      external: ['react', 'react-dom', 'react-p5-wrapper', 'next'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDom',
+          'react-p5-wrapper': 'ReactP5Wrapper',
+          'next': 'Next'
+        },
+      },
+    },
   }
 })
